44cc3781135a315b97c71c8c06d1d1ced92965ae
hayato <umai-beer@hotmail.co.jp>
2025-03-14 12:27:41 +0900

    ok

---
+++ PdfToImage/PdfToImageTests/PdfItemPageTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PdfToImage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfToImage.Tests
+{
+    [TestClass()]
+    public class PdfItemPageTests
+    {
+        [TestMethod()]
+        public async void SaveImageAsyncTest()
+        {
+            var dirDirPath = "C:\\Users\\htakahashi\\Downloads\\";
+            var files = System.IO.Directory.GetFiles(dirDirPath, "*.pdf");
+            if (files is null || files.Length == 0) Assert.Fail();
+            foreach(var file in files)
+            {
+                var pdf = new PdfItem(file);
+                await pdf.InitPageItemAsync();
+                pdf.
+            }
+            Assert.Fail();
+        }
+    }
+}
