0f481b99e3bbcd3bb8ef1578ab6e106c40d29400
umai-beer <umai-beer@hotmail.co.jp>
2025-03-15 22:04:05 +0900

    ok

--- ImageShredded/ImageShredded/BmpShredded.cs
+++ ImageShredded/ImageShredded/BmpShredded.cs
@@ -3,24 +3,40 @@ using System.Drawing.Imaging;
 
 namespace ImageShredded
 {
-    public class BmpShredded
+    /// <summary>
+    /// BitMapイメージの千切り
+    /// </summary>
+    public static class BmpShredded
     {
-        public IEnumerable<Task<Bitmap?>>  GetHorizotalShreddedAsync<TResult>(Bitmap image, int height)
+        /// <summary>
+        /// 縦方向へ千切り
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="width">縦に切る幅</param>
+        /// <returns></returns>
+        public static IEnumerable<Image> GetVirticalShredded(Bitmap image, int width)
         {
-            if (height > image.Height)
+            if (width > image.Width)
             {
                 yield break;
             }
-            var loopN = image.Height / height + (image.Height == height ? 0 : 1);
-            
-            var tasks = new List<Task<TResult>>(loopN);
+            var loopN = image.Width / width + (image.Width == width ? 0 : 1);
             for (var index = 0; loopN >= index; index++)
             {
-                yield return Task.Run(() => GetTrimHorizontal(image, index * height, height));
+                var trim = GetTrimVirtical(image, index * width, width);
+                if (trim is not null)
+                {
+                    yield return trim;
+                }
             }
         }
-
-        public IEnumerable<Image> GetHorizotalShredded(Bitmap image, int height)
+        /// <summary>
+        /// /横方向へ千切り
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="height">横に切る高さ</param>
+        /// <returns></returns>
+        public static IEnumerable<Image> GetHorizotalShredded(Bitmap image, int height)
         {
             if (height > image.Height)
             {
@@ -36,18 +52,48 @@ namespace ImageShredded
                 }
             }
         }
-
-        private Bitmap? GetTrimHorizontal(Bitmap image,int startPixY,int height)
+        /// <summary>
+        /// 縦方向へ千切り
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="startPixY"></param>
+        /// <param name="height">縦に切る幅</param>
+        /// <returns></returns>
+        internal static Bitmap? GetTrimHorizontal(Bitmap image, int startPixY, int height)
         {
-            if(startPixY >= image.Height)
+            if (startPixY >= image.Height)
             {
                 return null;
             }
-            var trimHeight=startPixY + height > image.Height ? image.Height - startPixY:height;
+            var trimHeight = startPixY + height > image.Height ? image.Height - startPixY : height;
             return GetTrimBitmap(image, 0, startPixY, image.Width, trimHeight);
         }
-
-        private Bitmap? GetTrimBitmap(Bitmap image,int pixX,int pixY,int width,int heigh)
+        /// <summary>
+        /// 横方向へ千切り
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="startPixX"></param>
+        /// <param name="width">横に切る高さ</param>
+        /// <returns></returns>
+        internal static Bitmap? GetTrimVirtical(Bitmap image,int startPixX,int width)
+        {
+            if (startPixX >= image.Width)
+            {
+                return null;
+            }
+            var trimWidth = startPixX + width > image.Width ? image.Width - startPixX : width;
+            return GetTrimBitmap(image, startPixX, 0, trimWidth, image.Height);
+        }
+        /// <summary>
+        /// BitMapを任意範囲に切り取り
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="pixX"></param>
+        /// <param name="pixY"></param>
+        /// <param name="width"></param>
+        /// <param name="heigh"></param>
+        /// <returns></returns>
+        internal static Bitmap? GetTrimBitmap(Bitmap image,int pixX,int pixY,int width,int heigh)
         {
             if( 0 > pixX || pixX > image.Width || pixX +width > image.Width ||
                 0 > pixY || pixY >image.Height || pixY + heigh >image.Height)
@@ -55,6 +101,7 @@ namespace ImageShredded
                 return null;
             }
             var rect = new Rectangle(pixX, pixY, width, heigh);
+            
             return image.Clone(rect,PixelFormat.DontCare);
         }
     }
---
+++ ImageShredded/ImageShreddedTests/BmpShreddedTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageShredded;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Reflection;
+
+namespace ImageShredded.Tests
+{
+    [TestClass()]
+    public class BmpShreddedTests
+    {
+        [TestMethod()]
+        public void GetHorizotalShreddedTest()
+        {
+            try
+            {
+                var imagePath = "F:\\data\\barcodetest.bmp";
+                var outDir = "F:\\data\\testout";
+
+                var bitmap = new Bitmap(imagePath);
+                var list = BmpShredded.GetHorizotalShredded(bitmap, 5);
+                var index = 1;
+                foreach (var item in list)
+                {
+                    item.Save(System.IO.Path.Combine(outDir, $"test-{index}.bmp"));
+                    index++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+        }
+
+    }
+}
